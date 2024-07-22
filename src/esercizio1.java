c796bce62717e8ad0ef0e2e5bdda3b1b91017003
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-07-22 15:25:16 +0200

    esercizio 1 (1 e 2)

--- src/esercizio1.java
+++ src/esercizio1.java
@@ -1,5 +1,15 @@
 public class esercizio1 {
     public static void main(String[] args) {
-        
+        System.out.println(moltiplicazione(2,3));
+        System.out.println(esercizio2("ciao", 2));
     }
+
+
+    public static  int moltiplicazione(int n1, int n2){
+        return n1*n2;
+    }
+    public static String esercizio2( String s1,int n1){
+        return s1+ String.valueOf(n1);
+    }
+
 }
