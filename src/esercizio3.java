94052bd018e11306ff7fcbab075783b535b0ec3e
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-07-22 17:58:24 +0200

    finito esercizio 3

--- src/esercizio3.java
+++ src/esercizio3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class esercizio3 {
     public static void main(String[] args) {
-        pariDispari();
+        areaRettangolo();
 
 
 
@@ -12,9 +12,9 @@ public class esercizio3 {
     public static void rettangolo(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("inserisci primo lato");
-        Double base = scanner.nextDouble();
+        Double base = Double.parseDouble(scanner.nextLine());
         System.out.println("inserisci secondo lato");
-        Double altezza = scanner.nextDouble();
+        Double altezza = Double.parseDouble(scanner.nextLine());
         System.out.println("il perimetro del rettangolo è: " + calcolaPerimetro(base,altezza));
 
     }
@@ -37,4 +37,24 @@ return (par1 + par2)*2;
     public static int calcolaPariDispari (int numero){
         return numero%2;
     }
+
+    public static void areaRettangolo(){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("inserisci primo lato");
+        double lato1= Double.parseDouble(scanner.nextLine());
+        System.out.println(";inserisci secondo lato");
+        double lato2= Double.parseDouble(scanner.nextLine());
+        System.out.println("inserisci terzo lato");
+        double lato3= Double.parseDouble(scanner.nextLine());
+        System.out.println("l'area del rettangolo è: "+ calcolaAreaRettangolo(lato1,lato2,lato3));
+
+
+    }
+
+    public static double calcolaAreaRettangolo(double lato1,double lato2,double lato3){
+          double s = (lato1 + lato2 + lato3) / 2;
+
+        return Math.sqrt(s * (s - lato1) * (s - lato2) * (s - lato3));
+    }
+
 }
